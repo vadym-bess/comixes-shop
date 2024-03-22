6bff3b577907a0d9be3700aed7764525bda7b987
Vadym Besarab <besarabvadim2@gmail.com>
2024-03-22 18:08:42 +0200

    final commit

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './cart.module.scss';
 import image from '../../components/images/eagle.jpg';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Cart() {
     const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function Cart() {
     }
     return(
         <div className={styles.cartThumb}>
+            
             <div className={styles.cartInnerThumb}>
                 <div className={styles.titleThumb}>
                    <h2>গোপনীয়তা নীতি</h2>
@@ -26,24 +28,24 @@ function Cart() {
 
                 <div className={styles.cartImageThumb}>
                     <div className={styles.cartImageSmallThumb}>
-                        <p className={styles.imageTitle}>পণ্য</p>
+                        <p className={styles.imageTitle}>পণ্য Product</p>
                         <div className={styles.imageThumb}>
                              <img src={image} alt="comix" />
                         </div>
                         
                     </div>
-                    <p className={styles.comixName}>দিগন্তীয় সামগ্রী</p>
+                    <p className={styles.comixName}>দিগন্তীয় সামগ্রী Horizontal content</p>
                     <span className={styles.close}>&times;</span>
                     
                 </div>
 
                     <div className={styles.idThumb}>
-                        <p className={styles.imageTitle}>মূল্য</p>
+                        <p className={styles.imageTitle}>মূল্য price</p>
                         <p className={styles.comixName}>BDT 3439</p>
                     </div>
 
                     <div className={styles.idThumb}>
-                        <p className={styles.imageTitle}>পরিমাণ</p>
+                        <p className={styles.imageTitle}>পরিমাণ Amount</p>
                         <div className={styles.calculator}>
                             <div onClick={minus} className={styles.minus}>-</div>
                             <div className={styles.sum}>{count}</div>
@@ -52,12 +54,15 @@ function Cart() {
                     </div>
 
                     <div className={styles.idThumb}>
-                        <p className={styles.imageTitle}>মোট</p>
+                        <p className={styles.imageTitle}>মোট total</p>
                         <p className={styles.comixName}>BDT 3439</p>
                     </div>
 
                     <div className={styles.buttonsThumb}>
+                        <Link to="/">
                         <button className={styles.whiteButton}>কেনাকাটা চালিয়ে যান</button>
+
+                        </Link>
                         <button className={styles.redButton}>পর্যালোচনা</button>
                     </div>
                
@@ -67,3 +72,7 @@ function Cart() {
 }
 
 export default Cart;
+
+
+
+        
