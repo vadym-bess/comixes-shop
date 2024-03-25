0aab6bc55d162a7068ee1d58a63f61ca8ec500fd
Vadym Besarab <besarabvadim2@gmail.com>
2024-03-25 14:48:43 +0200

    some changes

--- src/components/headerMob/index.jsx
+++ src/components/headerMob/index.jsx
@@ -4,21 +4,33 @@ import burgerMenu from '../../components/images/burger-menu.png';
 import closeMenu from '../../components/images/close-header.png'; // Добавляем изображение для закрытого меню
 import styles from './headerMob.module.scss';
 import cart from '../../components/images/cart-simple.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function HeaderMob() {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const location = useLocation();
 
     const toggleModal = () => {
         setIsModalOpen(prevState => !prevState);
     };
 
+    // Функция для закрытия модального окна и прокрутки к якорю
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    // Проверяем, находимся ли мы на странице корзины
+    const isCartPage = location.pathname === '/cart';
+
     return (
         <div className={styles.headerMob}>
-            <button className={styles.headerButton} type='button' onClick={toggleModal}>
-                {/* Используем условный оператор для выбора изображения */}
-                <img className={styles.burgerMenuImage} src={isModalOpen ? closeMenu : burgerMenu} alt="logo" />
-            </button>
+            {/* Показываем кнопку бургера только если мы НЕ находимся на странице корзины */}
+            {!isCartPage && (
+                <button className={styles.headerButton} type='button' onClick={toggleModal}>
+                    {/* Используем условный оператор для выбора изображения */}
+                    <img className={styles.burgerMenuImage} src={isModalOpen ? closeMenu : burgerMenu} alt="logo" />
+                </button>
+            )}
             <Link to="/">
                 <img className={styles.logo} src={logo} alt="logo" />
             </Link>
@@ -30,12 +42,10 @@ function HeaderMob() {
             {isModalOpen && (
                 <div className={styles.modal}>
                     {/* Содержимое модального окна */}
-                    <a href="#aboutUs" className={styles.navLink} to="#">আমাদের সম্পর্কে</a>
-                    <a href="#comixes" className={styles.navLink} to="#">দোকান</a>
-                    <a href="#comunication" className={styles.navLink} to="#">পর্যালোচনা</a>
-                    <a href="#comunication" className={styles.navLink} to="#">যোগাযোগ</a>
-
-
+                    <a href="#aboutUs" className={styles.navLink} onClick={handleCloseModal}>আমাদের সম্পর্কে</a>
+                    <a href="#comixes" className={styles.navLink} onClick={handleCloseModal}>দোকান</a>
+                    <a href="#comunication" className={styles.navLink} onClick={handleCloseModal}>পর্যালোচনা</a>
+                    <a href="#comunication" className={styles.navLink} onClick={handleCloseModal}>যোগাযোগ</a>
                 </div>
             )}
         </div>
--- src/components/headerWeb/index.jsx
+++ src/components/headerWeb/index.jsx
@@ -1,22 +1,27 @@
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './headerWeb.module.scss';
 import logo from '../../components/images/Logo.png';
 import cart from '../../components/images/cart-simple.png';
 
 function HeaderWeb({ cartItems }) {
+    const location = useLocation();
+    const isCartPage = location.pathname === '/cart';
+
     return (
         <div className={styles.headerWeb}>
             <Link to="/">
                 <img className={styles.logo} src={logo} alt="logo" />
             </Link>
-            <nav className={styles.navigationThumb}>
-                <a href="#aboutUs" className={styles.navLink} to="#">আমাদের সম্পর্কে</a>
-                <a href="#comixes" className={styles.navLink} to="#">দোকান</a>
-                <a href="#comunication" className={styles.navLink} to="#">পর্যালোচনা</a>
-                <a href="#comunication" className={styles.navLink} to="#">যোগাযোগ</a>
-            </nav>
+            {/* Показываем навигацию только если мы НЕ находимся на странице корзины */}
+            {!isCartPage && (
+                <nav className={styles.navigationThumb}>
+                    <a href="#aboutUs" className={styles.navLink} to="#">আমাদের সম্পর্কে</a>
+                    <a href="#comixes" className={styles.navLink} to="#">দোকান</a>
+                    <a href="#comunication" className={styles.navLink} to="#">পর্যালোচনা</a>
+                    <a href="#comunication" className={styles.navLink} to="#">যোগাযোগ</a>
+                </nav>
+            )}
             <Link to="/cart">
                 <div className={styles.cartThumb}>
                     <img className={styles.cart} src={cart} alt="logo" />
